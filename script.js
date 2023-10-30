b51eac35fcb2e2ff00f2226606c111cce76994fa
John Doe <johndoe@example.com>
2023-10-30 14:26:26 +0100

    alle sider lagt inn

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
-console.log("hei");
+
+
+
+
+const submitInfoEl = document.querySelector("#submitInfo")
+const infoElArr = document.querySelectorAll(".info")
+const infoDivEl = document.querySelector("#infoDiv")
+
+let infoArr = []
+
+
+const vareVelgingDivEl = document.querySelector("#vareVelging")
+const vareElArr = document.querySelectorAll(".vare")
+const leggTilButtonEl = document.querySelector("#leggTil")
+const submitVarerEl = document.querySelector("#submitVarer")
+
+const varerArr = []
+
+
+const kvitteringDivEl = document.querySelector("#kvittering")
+
+
+submitInfoEl.onclick = () => {
+    const infoElArrCopy = [...infoElArr]
+
+    infoArr = infoElArrCopy.map(e => e.value)
+
+    infoDivEl.style.display = "none"
+    vareVelgingDivEl.style.display = "flex"
+}
+
+leggTilButtonEl.onclick = () => {
+    const vareElArrCopy = [...vareElArr]
+
+    const vareValues = vareElArrCopy.map(e => e.value)
+
+    varerArr.push(vareValues)
+
+    vareElArr.forEach(element => {
+        element.value = ""
+    })
+}
+
+function updateKvittering() {
+
+    kvitteringDivEl.innerHTML = "heihei"
+
+}
+
+submitVarerEl.onclick = () => {
+    
+    vareVelgingDivEl.style.display = "none"
+    
+    updateKvittering()
+
+    kvitteringDivEl.style.display = "flex"
+
+
+}
